a1e3f4fe86cb58cfcea2fd93a99ac14de510d58a
tan27 <tn2706@gmail.com>
2021-08-02 12:48:22 -0700

    particles

--- src/App.js
+++ src/App.js
@@ -12,12 +12,18 @@ import './App.css';
 
 const particlesOptions = {
   particles: {
-    number: {
-      value: 50,
-      density: {
-        enable:true,
-        value_area: 800
-      }
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
     },
     move: {
       direction: "none",
@@ -26,8 +32,25 @@ const particlesOptions = {
       random: false,
       speed: 6,
       straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    }
   }
- }
 }
 
 const initialState = {
