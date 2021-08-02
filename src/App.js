286b5e2e36109c1ee9b78cff0476491873f7b452
tan27 <tn2706@gmail.com>
2021-08-02 12:22:27 -0700

    specify node

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
+// import Particles from 'react-particles-js';
 import Particles from "react-tsparticles";
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
